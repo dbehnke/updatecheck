2284713a5c9ac3d41813022ff27139d2f5225785
Dave Behnke <dbehnke74@gmail.com>
2017-09-10 17:24:14 -0400

    fix main.go, add .gitignore and .gitlab-ci.yml

--- cmd/updatecheck/main.go
+++ cmd/updatecheck/main.go
@@ -4,99 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 
-	logxi "github.com/mgutz/logxi/v1"
 	"gitlab.com/dbehnke74/updatecheck"
 	yaml "gopkg.in/yaml.v2"
 )
-
-//2017.07.27
-const versionjre = "1.8.0.144"
-
-//2017.07.11
-const versionfirefox = "52.3.0"
-
-//2017.08.08
-const versionflash = "26.0.0.151"
-
-//2017.08.16
-const versionoffice2016 = "15.37.0 [170815] 64-bit"
-
-func olddetect() {
-	log := logxi.New("olddetect")
-	var check string
-	var exitcode int
-	if len(os.Args) > 1 {
-		check = strings.ToLower(os.Args[1])
-		log.Debug("check", "check", check)
-	}
-	checkall := strings.HasPrefix(check, "all")
-	checkjre := strings.Contains(check, "jre") || checkall
-	if checkjre {
-		u, err := updatecheck.OracleJRE()
-		if err != nil {
-			log.Error("Error Checking OracleJRE", "err", err)
-			exitcode = 1
-		} else {
-			fmt.Println(u.Product, u.CurrentVersion, versionjre)
-			if u.CurrentVersion != versionjre {
-				log.Warn("Update Detected", "current", u.CurrentVersion,
-					"check", versionjre)
-				exitcode = 2
-			}
-		}
-	}
-
-	checkflash := strings.Contains(check, "flash") || checkall
-	if checkflash {
-		u, err := updatecheck.AdobeFlashPlayer()
-		if err != nil {
-			log.Error("Error Checking AdobeFlashPlayer", "err", err)
-			exitcode = 1
-		} else {
-			fmt.Println(u.Product, u.CurrentVersion, versionflash)
-			if u.CurrentVersion != versionflash {
-				log.Warn("Update Detected", "current", u.CurrentVersion,
-					"check", versionflash)
-				exitcode = 2
-			}
-		}
-	}
-
-	checkfirefox := strings.Contains(check, "firefox") || checkall
-	if checkfirefox {
-		s := updatecheck.NewFirefoxESR("mac")
-		if !s.Refresh() {
-			log.Error("Error Checking FirefoxESR")
-			exitcode = 1
-		} else {
-			fmt.Println(s.Name, s.CurrentVersion(), versionfirefox)
-			if s.CurrentVersion() != versionfirefox {
-				log.Warn("Update Detected", "current", s.CurrentVersion(),
-					"check", versionfirefox)
-				exitcode = 2
-			}
-		}
-	}
-
-	checkoffice2016 := strings.Contains(check, "office2016") || checkall
-	if checkoffice2016 {
-		u, err := updatecheck.Office2016()
-		if err != nil {
-			log.Error("Error Checking Office2016", "err", err)
-			exitcode = 1
-		} else {
-			fmt.Println(u.Product, u.CurrentVersion, versionoffice2016)
-			if u.CurrentVersion != versionoffice2016 {
-				log.Warn("Update Detected", "current", u.CurrentVersion,
-					"check", versionoffice2016)
-				exitcode = 2
-			}
-		}
-	}
-	os.Exit(exitcode)
-}
 
 func main() {
 	if len(os.Args) == 1 {
