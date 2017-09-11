90167219df0eb219e7b2079fafb760afceefa495
Dave Behnke <dbehnke74@gmail.com>
2017-09-11 14:28:56 -0400

    add a crude updatecheck (check.sh) until can do something proper

---
+++ check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e -x
+export CURRENTDIR=$PWD
+export GOPATH=/tmp/updatecheck/go
+export PROJECTPATH=$GOPATH/src/gitlab.com/dbehnke74/updatecheck
+mkdir -p $PROJECTPATH || true
+cp -R * $PROJECTPATH
+go build cmd/updatecheck/main.go
+./main results
+diff -c1 checkresults.json results.json
+
